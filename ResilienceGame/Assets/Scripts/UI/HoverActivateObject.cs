29319052b9df52f0887d9f012f87a758c7128e8d
Dariel Ravelo-Ramos <15proviso@gmail.com>
2024-07-26 10:20:15 -0400

    added the hover functionality to the facilities

--- ResilienceGame/Assets/Scripts/UI/HoverActivateObject.cs
+++ ResilienceGame/Assets/Scripts/UI/HoverActivateObject.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -5,9 +6,12 @@ using UnityEngine.EventSystems;
 
 public class HoverActivateObject : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
 {
-    public GameObject targetObject;
     public float delay = 0.5f; 
     public float maxHeightOffset = 100;
+    public Boolean highlight = false;
+    public GameObject highlightObject;
+    public Boolean scale = false;
+    public GameObject scaleObject;
     private Vector3 originalPosition;
     private float timer = 0; 
     private bool isHovering = false; 
@@ -22,20 +26,27 @@ public class HoverActivateObject : MonoBehaviour, IPointerEnterHandler, IPointer
         //will scale the object that is being hovered over and will count a timer until it shows extra card info. 
         if (isHovering)
         {
-            if (!isScaled) ScaleCard(.5f);
-
-            timer += Time.deltaTime;  
-            if (timer >= delay)
+            if (!isScaled & scale)
             {
-                targetObject.SetActive(true);
-                if(targetObject.transform.localPosition.y < originalPosition.y + maxHeightOffset)
+                ScaleCard(.5f);
+
+                timer += Time.deltaTime;
+                if (timer >= delay)
                 {
-                    targetObject.transform.localPosition += new Vector3(0, 1, 0);
+                    // hoverObject.SetActive(true);
+                    if (scaleObject.transform.localPosition.y < originalPosition.y + maxHeightOffset)
+                    {
+                        scaleObject.transform.localPosition += new Vector3(0, 1, 0);
+                    }
                 }
             }
+            if (highlight) highlightObject.SetActive(true);
         }
         //toggles the scaling effect to scale it back to its original size
-        else if (isScaled) ScaleCard(-.5f);
+        else
+        { if (isScaled & scale) ScaleCard(-.5f); 
+          if (highlight) highlightObject.SetActive(false);
+        }
     }
 
     public void ScaleCard(float scaleAmount)
@@ -47,14 +58,14 @@ public class HoverActivateObject : MonoBehaviour, IPointerEnterHandler, IPointer
             this.gameObject.layer = 6;
         }
 
-        Vector2 tempScale = targetObject.transform.parent.localScale;
-        Vector3 offset = targetObject.transform.parent.localPosition;
-        tempScale.x = (float)(targetObject.transform.parent.localScale.x + scaleAmount);
-        tempScale.y = (float)(targetObject.transform.parent.localScale.y + scaleAmount);
+        Vector2 tempScale = scaleObject.transform.parent.localScale;
+        Vector3 offset = scaleObject.transform.parent.localPosition;
+        tempScale.x = (float)(scaleObject.transform.parent.localScale.x + scaleAmount);
+        tempScale.y = (float)(scaleObject.transform.parent.localScale.y + scaleAmount);
         offset.y = offset.y + scaleAmount * 200;
 
-        targetObject.transform.parent.localScale = tempScale;
-        targetObject.transform.parent.localPosition = offset;
+        scaleObject.transform.parent.localScale = tempScale;
+        scaleObject.transform.parent.localPosition = offset;
         
         isScaled = !isScaled;
     }
@@ -68,7 +79,10 @@ public class HoverActivateObject : MonoBehaviour, IPointerEnterHandler, IPointer
     {
         isHovering = false;       
         timer = 0;
-        targetObject.transform.SetLocalPositionAndRotation(new Vector3(), targetObject.transform.rotation);
-        targetObject.SetActive(false);
+        if (scale)
+        {
+            scaleObject.transform.SetLocalPositionAndRotation(new Vector3(), scaleObject.transform.rotation);
+            //scaleObject.SetActive(false);
+        }
     }
 }
